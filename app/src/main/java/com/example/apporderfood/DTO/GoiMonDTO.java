25df33d2efa145dfb04ac6167d4f8bd1c02f525b
M26644 <89495096+M26644@users.noreply.github.com>
2021-10-15 15:40:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/apporderfood/DTO/GoiMonDTO.java
@@ -0,0 +1,55 @@
+package com.example.apporderfood.DTO;
+
+public class GoiMonDTO {
+    int MaNV;
+    String TinhTrang,NgayGoi;
+    int MaGoiMon;
+    int MaBan;
+
+
+    public int getMaNV() {
+        return MaNV;
+    }
+
+    public void setMaNV(int maNV) {
+        MaNV = maNV;
+    }
+
+
+    public String getTinhTrang() {
+        return TinhTrang;
+    }
+
+    public void setTinhTrang(String tinhTrang) {
+        TinhTrang = tinhTrang;
+    }
+
+    public String getNgayGoi() {
+        return NgayGoi;
+    }
+
+    public void setNgayGoi(String ngayGoi) {
+        NgayGoi = ngayGoi;
+    }
+
+    public int getMaGoiMon() {
+        return MaGoiMon;
+    }
+
+    public void setMaGoiMon(int maGoiMon) {
+        MaGoiMon = maGoiMon;
+    }
+
+    public int getMaBan() {
+        return MaBan;
+    }
+
+    public void setMaBan(int maBan) {
+        MaBan = maBan;
+    }
+
+
+
+
+
+}
